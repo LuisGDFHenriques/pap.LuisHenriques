a32fa78ac0d999b9d7b7d9968fb1fae0b01ded01
LuisGDFHenriques <luisdeushenriques@gmail.com>
2021-04-21 09:24:47 +0100

    Update

--- admin/confirmaEditaTelemovel.php
+++ admin/confirmaEditaTelemovel.php
@@ -1,16 +1,16 @@
 <?php
 include_once ("includes/body.inc.php");
 
-$id=intval($_POST['telemovelId']);
 $modelo=addslashes($_POST['modeloTelemovel']);
-$descricao=addslashes($_POST['descricaoTelemovel']);
-$idMar=intval($_POST['telemovelMarca']);
-$imagem=$_FILES['imagemTelemovel']['name'];
+$id=intval($_POST['telemovelId']);
+$descricao=addslashes($_POST['reviewTexto']);
+$idMar=intval($_POST['telemovelMarcaId']);
+$imagem=$_FILES['imagemTelemovel']['tmp_name'];
 $novoNome="images/".$imagem;
 $sql="UPDATE telemoveis SET telemovelModelo='".$modelo."', telemovelMarcaId='".$idMar."', telemovelDescricao='".$descricao."'";
 if($imagem!=''){
-    $sql.=", telemovelImagemURL='images/".$imagem."'";
-    copy($_FILES['imagemTelemovel']['tmp_name'],"../".$novoNome);
+    $sql.=", telemovelImagemURL = 'images/".$imagem."'";
+    copy($_FILES['imagemTelemovel']['name'],$novoNome);
 }
 mysqli_query($con,$sql);
 header("location:telemoveis.php");
--- admin/editarTelemoveis.php
+++ admin/editarTelemoveis.php
@@ -3,11 +3,73 @@ include_once ("includes/body.inc.php");
 top();
 
 $id=intval($_GET['id']);
-$sql="Select * from telemoveis inner join marcas on telemovelMarcaId = marcaId where telemovelId=$id";
+$sql="Select * from telemoveis inner join marcas on telemovelMarcaId = marcaId ";
 $result=mysqli_query($con,$sql);
 $dados=mysqli_fetch_array($result);
 
 ?>
+
+    <link href="summernote.css" rel="stylesheet">
+    <script src='js/tinymce/tinymce.min.js'></script>
+
+    <script>
+
+        tinymce.init({
+            selector: 'textarea#myTextarea',
+            plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+            imagetools_cors_hosts: ['picsum.photos'],
+            menubar: 'file edit view insert format tools table help',
+            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+            toolbar_sticky: true,
+            autosave_ask_before_unload: true,
+            autosave_interval: "30s",
+            autosave_prefix: "{path}{query}-{id}-",
+            autosave_restore_when_empty: false,
+            autosave_retention: "2m",
+            image_advtab: true,
+            /*content_css: '//www.tiny.cloud/css/codepen.min.css',*/
+            link_list: [
+                { title: 'My page 1', value: 'https://www.codexworld.com' },
+                { title: 'My page 2', value: 'https://www.xwebtools.com' }
+            ],
+            image_list: [
+                { title: 'My page 1', value: 'https://www.codexworld.com' },
+                { title: 'My page 2', value: 'https://www.xwebtools.com' }
+            ],
+            image_class_list: [
+                { title: 'None', value: '' },
+                { title: 'Some class', value: 'class-name' }
+            ],
+            importcss_append: true,
+            file_picker_callback: function (callback, value, meta) {
+                /* Provide file and text for the link dialog */
+                if (meta.filetype === 'file') {
+                    callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                }
+                /* Provide image and alt text for the image dialog */
+                if (meta.filetype === 'image') {
+                    callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                }
+                /* Provide alternative source and posted for the media dialog */
+                if (meta.filetype === 'media') {
+                    callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                }
+            },
+            templates: [
+                { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+            ],
+            template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+            template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+            height: 600,
+            image_caption: true,
+            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            noneditable_noneditable_class: "mceNonEditable",
+            toolbar_mode: 'sliding',
+            contextmenu: "link image imagetools table",
+        });
+    </script>
     <div class="services">
         <div class="container">
             <div class="row">
@@ -19,14 +81,14 @@ $dados=mysqli_fetch_array($result);
                         <input type="hidden" name="telemovelId" value="<?php echo $id?>">
                         <label for="modeloTelemovel">Nome: </label>
                         <input type="text" class="form-control" id="modeloTelemovel" name="modeloTelemovel" value="<?php echo $dados['telemovelModelo']?>"><br>
-                        <label for="descricaoTelemovel">Descrição: </label>
-                        <textarea class="form-control" id="descricaoTelemovel" name="descricaoTelemovel"><?php echo $dados['telemovelDescricao']?></textarea><br>
+                        <label for="myTextarea">Descrição: </label>
+                        <textarea class="form-control" id="myTextarea" name="reviewTexto"><?php echo $dados['telemovelDescricao']?></textarea><br>
                         <img src="../<?php echo $dados['telemovelImagemURL']?>" width="200">
                         <label for="imagemTelemovel">Imagem: </label>
                         <input type="file" id="imagemTelemovel" name="imagemTelemovel"><br>
                         <br>
                         <br>
-                        <select name="telemovelMarca">
+                        <select name="telemovelMarcaId">
                             <option value="-1">Escolha a marca...</option>
                             <?php
                             $sql="select * from marcas order by marcaNome";
