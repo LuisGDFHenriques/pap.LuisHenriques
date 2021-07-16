fc3fc49aaa619cec4cfae27858a8950343e592af
Leandro <leandro.pereira@lizserve.pt>
2021-07-16 12:12:20 +0100

    [leandro] Comparativo.php

--- Comparativo.php
+++ Comparativo.php
@@ -1,6 +1,43 @@
 <?php
 include_once ("includes/body.inc.php");
 top(COMPARATIVO);
+/* ********************************   dados fixos para alterar ****/
+$id1=1;
+$id2=3;
+/*****************************************************/
+
+$sql="select * from produtos where produtoId=".$id1;
+$res=mysqli_query($con,$sql);
+$dadosPrd1=mysqli_fetch_array($res);
+
+$sql="select * from produtos where produtoId=".$id2;
+$res=mysqli_query($con,$sql);
+$dadosPrd2=mysqli_fetch_array($res);
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++   chaves
+$sql="select *
+from chaves left join categoriachaves on categoriaChaveId=chaveCategoriaChaveId
+ left join 
+(select * from produtos inner join produtochaves on produtoId = produtoChaveProdutoId where produtoChaveProdutoId=$id1) as tabela 
+on chaveId = tabela.produtoChaveChaveId
+
+order by categoriaChaveTipo asc ";
+$result1=mysqli_query($con,$sql);
+
+$sql="select *
+from chaves left join categoriachaves on categoriaChaveId=chaveCategoriaChaveId
+ left join 
+(select * from produtos inner join produtochaves on produtoId = produtoChaveProdutoId where produtoChaveProdutoId=$id2) as tabela 
+on chaveId = tabela.produtoChaveChaveId
+
+order by categoriaChaveTipo asc ";
+$result2=mysqli_query($con,$sql);
+
+
+
 ?>
     <!-- Page Content -->
     <div class="page-heading header-text">
@@ -15,41 +52,56 @@ top(COMPARATIVO);
 
     <div class="services">
       <div class="container">
-              <table>
-                <tr><th><h4>Smartphone SAMSUNG Galaxy Z Fold2 5G (7.6'' - 12 GB - 256 GB)</h4></th><th><h4>Smartphone SAMSUNG Galaxy Z Fold2 5G (7.6'' - 12 GB - 256 GB)</h4></th></tr>
-              <tr><th><img src="assets/images/SAMSUNGZFOLD2.jpg   " alt="" class="img-fluid wc-image"></th><th><img src="assets/images/IPHONE8.gif" alt="" class="img-fluid wc-image"></th></tr>
-              <tr><th><h4>Caracteristicas</h4></th></tr>
-              <tr><th><p align="justify"><b>Características Específicas</b></p></th></tr>
-              <tr><th><p>Sistema Operativo-Android</p></th><th><p>Sistema Operativo-Android</p></th></tr>
-                <tr><th><p>Versão Sistema Operativo-10</p></th><th><p>Versão Sistema Operativo-10</p></th></tr>
-                <tr><th><p>Processador-Snapdragon 865+</p></th><th><p>Processador-Snapdragon 865+</p></th></tr>
-                <tr><th><p>Velocidade Processador (GHz)-Snapdragon 865+ OctaCore (7nm)</p></th><th><p>Velocidade Processador (GHz)-Snapdragon 865+ OctaCore (7nm)</p></th></tr>
-                <tr><th><p>Núcleos-OctaCore</p></th><th><p>Núcleos-OctaCore</p></th></tr>
-                <tr><th><p align="justify"><b>Características Físicas</b></p></th></tr>
-                <tr><th><p>Cor-Bronze</p></th><th><p>Cor-Bronze</p></th></tr>
-                <tr><th><p>Peso-282 g</p></th><th><p>Peso-282 g</p></th></tr>
-                <tr><th><p>Altura-15.92 cm</p></th><th><p>Altura-15.92 cm</p></th></tr>
-                <tr><th><p>Largura-6.8 cm/12.82 cm</p></th><th><p>Largura-6.8 cm/12.82 cm</p></th></tr>
-                <tr><th><p>Profundidade-6.9 cm/16.8 cm</p></th><th><p>Profundidade-6.9 cm/16.8 cm</p></th></tr>
-                <tr><th><p align="justify"><b>Ecrã</b></p></th></tr>
-                <tr><th><p>Diagonal do Ecrã-7.6</p></th><th><p>Diagonal do Ecrã-7.6</p></th></tr>
-                <tr><th><p>Tecnologia do Ecrã-Dynamic AMOLED 2x</p></th><th><p>Tecnologia do Ecrã-Dynamic AMOLED 2x</p></th></tr>
-                <tr><th><p>Resolução de Ecrã-QHD+</p></th><th><p>Resolução de Ecrã-QHD+</p></th></tr>
-                <tr><th><p align="justify"><b>Memória</b></p></th></tr>
-                <tr><th><p>Memória RAM-12 GB</p></th><th><p>Memória RAM-12 GB</p></th></tr>
-                <tr><th><p>Memória Interna-256 GB</p></th><th><p>Memória Interna-256 GB</p></th></tr>
-                <tr><th><p align="justify"><b>Câmara</b></p></th></tr>
-                <tr><th><p>Nº de câmaras traseiras-3</p></th><th><p>Nº de câmaras traseiras-3</p></th></tr>
-                <tr><th><p>Nº de câmaras frontais-2</p></th><th><p>Nº de câmaras frontais-2</p></th></tr>
-                <tr><th><p>Resolução Câmara Traseira-12+ 12 + 12</p></th><th><p>Resolução Câmara Traseira-12+ 12 + 12</p></th></tr>
-                <tr><th><p>Resolução Câmara Frontal-10</p></th><th><p>Resolução Câmara Frontal-10</p></th></tr>
-                <tr><th><p align="justify"><b>Bateria</b></p></th></tr>
-                <tr><th><p>Potência da bateria-4500mAh</p></th><th><p>Potência da bateria-4500mAh</p></th></tr>
-                <tr><th><p align="justify"><b>Conexões</b></p></th></tr>
-                <tr><th><p>Tipo de Rede-5G</p></th><th><p>Tipo de Rede-5G</p></th></tr>
+        <table width="100%" class="table ">
+            <tr>
+                <th width="50%"><h4><?php echo $dadosPrd1['produtoNome']?></h4></th>
+                <th width="50%"><h4><?php echo $dadosPrd2['produtoNome']?></h4></th>
+            </tr>
+            <tr>
+              <th class="text-left"><img width="220" src="<?php echo $dadosPrd1['produtoImagemURL']?>" alt="" class="img-fluid wc-image"></th>
+              <th class="text-left"><img width="220" src="<?php echo $dadosPrd2['produtoImagemURL']?>" alt="" class="img-fluid wc-image"></th>
+            </tr>
+            <tr>
+              <th colspan="2"><h4>Caracteristicas</h4></th>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <table width="100%" class="table table-striped">
+                        <?php
+                        $categoria='';
+                        while($dadosPrd1=mysqli_fetch_array($result1) ){
+                            $dadosPrd2=mysqli_fetch_array($result2);
+                            if($categoria!=$dadosPrd1['categoriaChaveNome']){
+                                $categoria=$dadosPrd1['categoriaChaveNome'];
+                                ?>
+                                <tr>
+                                    <td colspan="2" width="45%" class="text-left ml-3"><strong><i><?php echo $dadosPrd2['categoriaChaveNome']?></i></strong></td>
+                                    <td width="10%">&nbsp;</td>
+                                    <td colspan="2" width="45%" class="text-left ml-3"><strong><i><?php echo $dadosPrd2['categoriaChaveNome']?></i></strong></td>
+                                    <td width="10%">&nbsp;</td>
 
-  </table>
-        </div> <!-- fim do container -->
+                                </tr>
+                                    <?php
+                            }
+                        ?>
+                            <tr>
+                                <td width="20%" class="text-left ml-3"><strong><?php echo $dadosPrd1['chaveNome']?>:</strong></td>
+                                <td width="25%" class="text-left ml-3"><?php echo $dadosPrd1['produtoChaveValor']?></td>
+                                <td width="10%">&nbsp;</td>
+                                <td width="20%" class="text-left ml-3"><strong><?php echo $dadosPrd2['chaveNome']?>:</strong></td>
+                                <td width="25%" class="text-left ml-3"><?php echo $dadosPrd2['produtoChaveValor']?></td>
+
+                            </tr>
+                        <?php
+                        }
+                        ?>
+
+                    </table>
+                </td>
+
+            </tr>
+        </table>
+      </div> <!-- fim do container -->
     </div>
 
 
